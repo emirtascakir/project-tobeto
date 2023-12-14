31f3ecb2c0c52dc5829f7c41c67b4e1cae5e8c55
emirtascakir <emirtascakir@icloud.com>
2023-12-14 14:55:58 +0300

    proje isterleri 2

---
+++ project_tobeto_2/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:project_tobeto/views/home_view.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: const ColorScheme.dark(
+    background: Color.fromARGB(255, 58, 55, 55),
+    primary: Color(0xFF9933FF),
+    secondary: Color.fromARGB(255, 22, 22, 22),
+  ),
+  useMaterial3: true,
+);
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: const ColorScheme.light(
+      background: Color.fromARGB(255, 228, 228, 228),
+      primary: Color(0xFF9933FF),
+      secondary: Color.fromARGB(255, 22, 22, 22)),
+  useMaterial3: true,
+);
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: const HomeView(),
+    theme: lightTheme,
+    darkTheme: darkTheme,
+    themeMode: ThemeMode.system,
+  ));
+}
